0865ed4224c2ac1771b3187d92b9a5cd24fa5463
dev-edna <240291139+dev-edna@users.noreply.github.com>
2025-10-29 00:47:41 +0900

    chore : routes - 페이지 정리

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,13 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+    index("routes/home.tsx"), 
+    route("/help", "routes/help.tsx"), 
+    route("/history", "routes/history.tsx"),
+    route("/insights", "routes/insights.tsx"),
+    route("/log", "routes/log.tsx"),
+    route("/profile", "routes/profile.tsx"),
+    route("/signIn", "routes/signIn.tsx"),
+    route("/signUp", "routes/signUp.tsx"),
+    route("/templates", "routes/templates.tsx")
+] satisfies RouteConfig;
